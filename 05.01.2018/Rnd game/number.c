1cfbbcbdcd49919640934afe1b310278885f14be
tenqz <smmartbiz@gmail.com>
2018-01-05 04:36:47 +0300

    Add function in random game

--- 05.01.2018/Rnd game/number.c
+++ 05.01.2018/Rnd game/number.c
@@ -2,18 +2,43 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main()
+int randoms()
 {
-	int n;
 	srand(time(NULL));
-	int rnd = 1 + rand() % (100 - 1);
-	printf("Введите число: \n");
-	scanf("%d", &n);
+	int rnd = 1 + rand() % (10 - 1);
+	return rnd;
+}
+
+void game(int rnd, int n)
+{
 	if (rnd > n)
 	{
 		printf("Рандомное %d больше чем ваше %d \n", rnd, n);
-	} else {
+	} else if (rnd < n){
 		printf("Рандомное %d меньше чем ваше %d \n", rnd, n);
+	} else {
+		printf("Рандомное %d равно вашему %d \n", rnd, n);
 	}
+}
+
+int scannumber()
+{
+	int n;
+	printf("Введите число от 1 до 10: \n");
+	scanf("%d", &n);
+	if (n > 1 && n < 10)
+	{
+		return n;
+	} else {
+		return scannumber();
+	}
+}
+
+int main()
+{
+	int n;
+	int rnd = randoms();
+	n = scannumber();
+	game(rnd,n);
 	return 0;
 }
